6b782f7ece1b3bcab31c82da1ea6e038e73cf0fe
David Nastase <davidwnastase@gmail.com>
2017-02-10 16:25:46 -0500

    Adding React Code for front end.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,23 +1,13 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String greetingForm(Model model) {
-        model.addAttribute("greeting", new Greeting());
-        return "greeting";
+    @RequestMapping("/")
+    public String index() {
+        return "/index.html";
     }
-
-    @PostMapping("/")
-    public String greetingSubmit(@ModelAttribute Greeting greeting) {
-        return "result";
-    }
-
 }
